d18d7c2c59dc9886fc8be36dcc769d023a6f6942
Pedro <guiedroliusti@gmail.com>
2025-05-19 09:42:52 -0300

    07- adicionadas imagens na pasta images - criação do objeto shows - criação do arquivo montar front.js - importação do objeto show

---
+++ js/mortarFront.js
@@ -0,0 +1,3 @@
+import { shows }from "./objetos.js";
+console.log(shows);
+// console.table(shows)
---
+++ js/objetos.js
@@ -0,0 +1,243 @@
+export const shows = [
+    {
+      artista: "Legião Urbana",
+      local: "Brasília",
+      data: "2025-06-05",
+      horario: "21:00",
+      tipo_ingresso: "inteira",
+      banner: "../images/image0.jpg"
+    },
+    {
+      artista: "Titãs",
+      local: "São Paulo",
+      data: "2025-06-10",
+      horario: "21:00",
+      tipo_ingresso: "meia entrada",
+      banner: "../images/image1.jpg"
+    },
+    {
+      artista: "Barão Vermelho",
+      local: "Rio de Janeiro",
+      data: "2025-06-15",
+      horario: "21:00",
+      tipo_ingresso: "vip",
+      banner: "../images/image2.jpg"
+    },
+    {
+      artista: "Os Paralamas do Sucesso",
+      local: "Recife",
+      data: "2025-06-20",
+      horario: "21:00",
+      tipo_ingresso: "premium",
+      banner: "../images/image3.jpg"
+    },
+    {
+      artista: "Skank",
+      local: "Belo Horizonte",
+      data: "2025-06-25",
+      horario: "21:00",
+      tipo_ingresso: "pista",
+      banner: "../images/image4.jpg"
+    },
+    {
+      artista: "Engenheiros do Hawaii",
+      local: "Curitiba",
+      data: "2025-07-01",
+      horario: "21:00",
+      tipo_ingresso: "meia entrada",
+      banner: "../images/image5.jpg"
+    },
+    {
+      artista: "Capital Inicial",
+      local: "Porto Alegre",
+      data: "2025-07-05",
+      horario: "21:00",
+      tipo_ingresso: "inteira",
+      banner: "../images/image6.jpg"
+    },
+    {
+      artista: "Ira!",
+      local: "Florianópolis",
+      data: "2025-07-10",
+      horario: "21:00",
+      tipo_ingresso: "vip",
+      banner: "../images/image7.jpg"
+    },
+    {
+      artista: "CPM 22",
+      local: "Salvador",
+      data: "2025-07-15",
+      horario: "21:00",
+      tipo_ingresso: "pista",
+      banner: "../images/image8.jpg"
+    },
+    {
+      artista: "O Rappa",
+      local: "Fortaleza",
+      data: "2025-07-20",
+      horario: "21:00",
+      tipo_ingresso: "premium",
+      banner: "../images/image9.jpg"
+    },
+    {
+      artista: "Charlie Brown Jr.",
+      local: "João Pessoa",
+      data: "2025-07-25",
+      horario: "21:00",
+      tipo_ingresso: "meia entrada",
+      banner: "../images/image0.jpg"
+    },
+    {
+      artista: "Detonautas",
+      local: "Maceió",
+      data: "2025-07-30",
+      horario: "21:00",
+      tipo_ingresso: "vip",
+      banner: "../images/image1.jpg"
+    },
+    {
+      artista: "Jota Quest",
+      local: "Natal",
+      data: "2025-08-04",
+      horario: "21:00",
+      tipo_ingresso: "pista",
+      banner: "../images/image2.jpg"
+    },
+    {
+      artista: "Pitty",
+      local: "Teresina",
+      data: "2025-08-09",
+      horario: "21:00",
+      tipo_ingresso: "premium",
+      banner: "../images/image3.jpg"
+    },
+    {
+      artista: "Raimundos",
+      local: "Aracaju",
+      data: "2025-08-14",
+      horario: "21:00",
+      tipo_ingresso: "meia entrada",
+      banner: "../images/image4.jpg"
+    },
+    {
+      artista: "Los Hermanos",
+      local: "Palmas",
+      data: "2025-08-19",
+      horario: "21:00",
+      tipo_ingresso: "inteira",
+      banner: "../images/image5.jpg"
+    },
+    {
+      artista: "NX Zero",
+      local: "Campo Grande",
+      data: "2025-08-24",
+      horario: "21:00",
+      tipo_ingresso: "vip",
+      banner: "../images/image6.jpg"
+    },
+    {
+      artista: "Tihuana",
+      local: "Boa Vista",
+      data: "2025-09-03",
+      horario: "21:00",
+      tipo_ingresso: "premium",
+      banner: "../images/image7.jpg"
+    },
+    {
+      artista: "Rosa de Saron",
+      local: "Macapá",
+      data: "2025-09-08",
+      horario: "21:00",
+      tipo_ingresso: "meia entrada",
+      banner: "../images/image8.jpg"
+    },
+    {
+      artista: "Camisa de Vênus",
+      local: "Rio Branco",
+      data: "2025-09-13",
+      horario: "21:00",
+      tipo_ingresso: "vip",
+      banner: "../images/image9.jpg"
+    },
+    {
+      artista: "Fresno",
+      local: "Manaus",
+      data: "2025-09-18",
+      horario: "21:00",
+      tipo_ingresso: "inteira",
+      banner: "../images/image0.jpg"
+    },
+    {
+      artista: "Papas da Língua",
+      local: "Belém",
+      data: "2025-09-23",
+      horario: "21:00",
+      tipo_ingresso: "pista",
+      banner: "../images/image1.jpg"
+    },
+    {
+      artista: "O Terno",
+      local: "São Luís",
+      data: "2025-09-28",
+      horario: "21:00",
+      tipo_ingresso: "premium",
+      banner: "../images/image2.jpg"
+    },
+    {
+      artista: "Scalene",
+      local: "Vitória",
+      data: "2025-10-03",
+      horario: "21:00",
+      tipo_ingresso: "vip",
+      banner: "../images/image3.jpg"
+    },
+    {
+      artista: "Supercombo",
+      local: "Porto Velho",
+      data: "2025-10-08",
+      horario: "21:00",
+      tipo_ingresso: "meia entrada",
+      banner: "../images/image4.jpg"
+    },
+    {
+      artista: "Ego Kill Talent",
+      local: "Goiânia",
+      data: "2025-10-13",
+      horario: "21:00",
+      tipo_ingresso: "pista",
+      banner: "../images/image5.jpg"
+    },
+    {
+      artista: "Dead Fish",
+      local: "São Paulo",
+      data: "2025-10-18",
+      horario: "21:00",
+      tipo_ingresso: "inteira",
+      banner: "../images/image6.jpg"
+    },
+    {
+      artista: "Vivendo do Ócio",
+      local: "Salvador",
+      data: "2025-10-23",
+      horario: "21:00",
+      tipo_ingresso: "premium",
+      banner: "../images/image7.jpg"
+    },
+    {
+      artista: "Malta",
+      local: "Curitiba",
+      data: "2025-10-28",
+      horario: "21:00",
+      tipo_ingresso: "vip",
+      banner: "../images/image8.jpg"
+    },
+    {
+      artista: "Cachorro Grande",
+      local: "Porto Alegre",
+      data: "2025-11-02",
+      horario: "21:00",
+      tipo_ingresso: "meia entrada",
+      banner: "../images/image9.jpg"
+    }
+  ];
+  
