51b226e1a9acfe2f87b41a0fe21922b02f201d96
Pedro <guiedroliusti@gmail.com>
2025-05-26 08:33:53 -0300

    10 - adicionando o loading lazy nas páginas

--- js/mortarFront.js
+++ js/mortarFront.js
@@ -11,7 +11,7 @@ shows.forEach(show => {
     <div class="col-md-6 col-lg-4 my-3">
             <div class="card">
                 <a href="" class="text-decoration-none text-dark">
-                    <img src="${show.banner}" class="card-img-top" alt="${show.artista}">
+                    <img src="${show.banner}" class="card-img-top" alt="${show.artista}" loading="lazy">
                     <div class="card-body">
                         <h4 class="my-3 bd-bottom">${show.artista}</h4>
                         <div class="d-flex justify-content-between">
